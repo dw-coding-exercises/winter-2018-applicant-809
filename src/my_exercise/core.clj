45de08ea3072cba94c78f520437be784f32f9e3d
Anonymous Applicant <anonymous@example.com>
2017-12-17 13:00:09 -0700

    Fix typo in dependencies

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -3,9 +3,12 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [my-exercise.home :as home]))
+            [my-exercise.home :as home]
+            [my_exercise.controllers.searches :as searches]
+            [my_exercise.models.search :as model]))
 
 (defroutes app
+  searches/routes
   (GET "/" [] home/page)
   (route/resources "/")
   (route/not-found "Not found"))
