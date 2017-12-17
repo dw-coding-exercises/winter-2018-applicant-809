c66a43dc35d99f3eb2a2d843fb620b4747130056
Anonymous Applicant <anonymous@example.com>
2017-12-17 13:16:35 -0700

    Add clj-http dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  [ring "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [compojure "1.6.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [clj-http "0.6.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler my-exercise.core/handler}
   :aliases {"submit" ["run" "-m" "my-exercise.submit"]})
