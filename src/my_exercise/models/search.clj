f3dc33dfd1bd7e1034e980ab68cfed415f800a15
Anonymous Applicant <anonymous@example.com>
2017-12-17 13:17:53 -0700

    Add search model with find-by-address

--- src/my_exercise/models/search.clj
+++ src/my_exercise/models/search.clj
@@ -1,1 +1,10 @@
-(ns my_exercise.models.search)
+(ns my_exercise.models.search
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+
+(defn find-by-address
+  "process submitted address"
+  [address]
+  (:body
+    ))
