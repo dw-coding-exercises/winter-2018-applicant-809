948a63c0918ec56aba739203f2ad1784b4137c19
Anonymous Applicant <anonymous@example.com>
2017-12-17 13:17:27 -0700

    Adjust file requirement and add create

--- src/my_exercise/controllers/searches.clj
+++ src/my_exercise/controllers/searches.clj
@@ -2,11 +2,16 @@
   (:require [compojure.core :refer [defroutes POST]]
             [clojure.string :as str]
             [ring.util.response :as ring]
-            [my_exercise.models.search :as model]))
+            [my_exercise.models.search :as model]
+            [my_exercise.views.searches.index :as search-index]))
 
 (defn index
-  []
+  [search-results]
   "Hello World")
 
+(defn create
+  [address]
+  (model/find-by-address address))
+
 (defroutes routes
-  (POST "/search" [] (index)))
+  (POST "/search" [address] (create address)))
